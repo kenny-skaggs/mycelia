64d2ea446ac159cd3cfe895bde60c7cf4acddc6d
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-05-19 12:22:54 -0500

    some refactoring for accessing the datastore

--- server/datastore.py
+++ server/datastore.py
@@ -1,22 +1,44 @@
 from google.cloud import ndb
 
-UNSET = 'VALUE_NOT_YET_SET'
-client = ndb.Client()
 
-
-class Settings(ndb.Model):
+class Settings:
     name = ndb.StringProperty()
     value = ndb.StringProperty()
 
     @classmethod
     def get(cls, name):
-        with client.context():
-            setting = Settings.query(Settings.name == name).get()
+        _EnvSettings.get(name)
+
+
+class DatastoreSettings:
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatastoreSettings, cls).__new__(cls)
+            cls._instance.unset_value = 'VALUE_NOT_YET_SET'
+            cls._instance.client = ndb.Client()
+        return cls._instance
+
+    @classmethod
+    def get(cls, name):
+        instance = DatastoreSettings()
+        with instance.client.context():
+            setting = _DataStoreRecord.query(_DataStoreRecord.name == name).get()
             if not setting:
-                setting = Settings(name=name, value=UNSET)
+                setting = _DataStoreRecord(name=name, value=instance.unset_value)
                 setting.put()
 
-            if setting.value == UNSET:
+            if setting.value == instance.unset_value:
                 raise Exception(f'Value for setting "{name}" has not been set yet.')
 
             return setting.value
+
+
+class _DataStoreRecord(ndb.Model):
+    name = ndb.StringProperty()
+    value = ndb.StringProperty()
+
+
+class _EnvSettings:
+    @classmethod
+    def get(cls, name):
+        return name
